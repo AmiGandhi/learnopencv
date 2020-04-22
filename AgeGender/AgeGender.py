008a772288540d956d0a8f777610e8e9d3477db8
Ami Gandhi <gandhi.am@husky.neu.edu>
2020-04-22 07:21:17 -0400

    added helper functions to load network and make predictions

--- AgeGender/AgeGender.py
+++ AgeGender/AgeGender.py
@@ -4,6 +4,19 @@ import math
 import time
 import argparse
 
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+def load_network(model, proto):
+    network = cv.dnn.readNet(model, proto)
+    return network
+
 def getFaceBox(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
     frameHeight = frameOpencvDnn.shape[0]
@@ -24,29 +37,41 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn, bboxes
 
+def predict_gender(gender_net):
+    genderNet.setInput(blob)
+    genderPreds = genderNet.forward()
+    gender = genderList[genderPreds[0].argmax()]
+    # print("Gender Output : {}".format(genderPreds))
+    print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+    return gender
+
+def predict_age(age_net):
+    ageNet.setInput(blob)
+    agePreds = ageNet.forward()
+    age = ageList[agePreds[0].argmax()]
+    print("Age Output : {}".format(agePreds))
+    print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+    return age
+
+def display_image_predictions(cv, frameFace, bbox, bboxgender, age):
+    label = "{},{}".format(gender, age)
+    cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+    cv.imshow("Age Gender Recognition", frameFace)
+    # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
 
 parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
 parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
 
 args = parser.parse_args()
 
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
-
-ageProto = "age_deploy.prototxt"
-ageModel = "age_net.caffemodel"
-
-genderProto = "gender_deploy.prototxt"
-genderModel = "gender_net.caffemodel"
+# Load network
+ageNet = load_network(ageModel, ageProto)
+genderNet = load_network(genderModel, genderProto)
+faceNet = load_network(faceModel, faceProto)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
-
-# Load network
-ageNet = cv.dnn.readNet(ageModel, ageProto)
-genderNet = cv.dnn.readNet(genderModel, genderProto)
-faceNet = cv.dnn.readNet(faceModel, faceProto)
 
 # Open a video file or an image file or a camera stream
 cap = cv.VideoCapture(args.input if args.input else 0)
@@ -58,31 +83,16 @@ while cv.waitKey(1) < 0:
     if not hasFrame:
         cv.waitKey()
         break
-
     frameFace, bboxes = getFaceBox(faceNet, frame)
     if not bboxes:
         print("No face Detected, Checking next frame")
         continue
-
     for bbox in bboxes:
         # print(bbox)
-        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
-
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),
+                    max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
         blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds = genderNet.forward()
-        gender = genderList[genderPreds[0].argmax()]
-        # print("Gender Output : {}".format(genderPreds))
-        print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
-
-        ageNet.setInput(blob)
-        agePreds = ageNet.forward()
-        age = ageList[agePreds[0].argmax()]
-        print("Age Output : {}".format(agePreds))
-        print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
-
-        label = "{},{}".format(gender, age)
-        cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
-        cv.imshow("Age Gender Demo", frameFace)
-        # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
+        gender = predict_gender(genderNet)
+        age = predict_age(ageNet)
+        display_image_predictions(cv, frameFace, bbox, gender, age)
     print("time : {:.3f}".format(time.time() - t))
